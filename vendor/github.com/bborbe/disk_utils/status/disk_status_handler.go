5c03412bdce210938ded72b2b1fd736d8f7f3275
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-19 18:52:02 +0200

    use deps

---
+++ vendor/github.com/bborbe/disk_utils/status/disk_status_handler.go
@@ -0,0 +1,40 @@
+package status
+
+import (
+	"fmt"
+	"github.com/golang/glog"
+	"io"
+	"net/http"
+)
+
+type statusHandler struct {
+	path string
+}
+
+func NewHandler(path string) *statusHandler {
+	s := new(statusHandler)
+	s.path = path
+	return s
+}
+
+func (s *statusHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	glog.V(4).Infof("get metrics for path %s", s.path)
+	usage, err := DiskUsage(s.path)
+	if err != nil {
+		glog.V(4).Infof("get metrics for path %s failed: %v", s.path, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	write(w, "available bytes", "diskstatus_bytestotal", usage.BytesTotal)
+	write(w, "used bytes", "diskstatus_bytesused", usage.BytesUsed)
+	write(w, "free bytes", "diskstatus_bytesfree", usage.BytesFree)
+	write(w, "available inodes", "diskstatus_inodestotal", usage.InodesTotal)
+	write(w, "used inodes", "diskstatus_inodesused", usage.InodesUsed)
+	write(w, "free inodes", "diskstatus_inodesfree", usage.InodesFree)
+}
+
+func write(w io.Writer, des string, name string, value uint64) {
+	fmt.Fprintf(w, "# HELP %s %s\n", name, des)
+	fmt.Fprintf(w, "# TYPE %s gauge\n", name)
+	fmt.Fprintf(w, "%s %d\n", name, value)
+}
